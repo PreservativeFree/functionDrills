124fe5eac1f39a22f52e110a3a5d06502a23e64d
Nelson Moreno <thegreyabove@gmail.com>
2022-03-08 09:30:45 -0500

    third commit

--- functionDrills.js
+++ functionDrills.js
@@ -359,26 +359,26 @@ const chocolateFrogPurchase2 = function(submitGold) {
 console.log(chocolateFrogPurchase2(4));
 
 ////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray = [0,1,2,3,4,7,3,5,8,9]
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, 
   write logic to determine if the array is in ascending order. The function should return true, 
   if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set 
   it equal to your function invoked. Use the sample array to test this function.
 */
-let largest = 0;
-let isAscending = false;
+let isAscending = true;
 const arrayAscending = function(myArray) {
-  for(let i = 0; i < myArray.length; i++) {
-      for(let y = 0; y < myArray.length; y++) {
-        if(myArray[i] < myArray[y]) {
-          largest = myArray[i];
-          isAscending = true;
+    for(let i = 0; i < myArray.length; i++) {
+        for(let y = i+1; y < myArray.length; y++) {
+            if(myArray[i] > myArray[y]) {
+                isAscending = false;
         }
       }
   }
   if(isAscending)
     return true;
+    else
+      return false
 }
 let arrayIsAscending = arrayAscending(sampleArray);
 console.log(arrayIsAscending);
